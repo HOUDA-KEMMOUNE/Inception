2e8dabacfb37c99671fb57b7a23505ebe492b145
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2026-08-09 06:56:59 +0100

    init.sh file works good !

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -2,7 +2,41 @@
 
 set -e
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+mkdir -p /run/mysqld
+chown mysql:mysql /run/mysqld
+
+# Start MariaDB temporarily
+mysqld --user=mysql --skip-networking &
+
+# Wait until MariaDB is ready
+until mariadb -u root -e "SELECT 1;" >/dev/null 2>&1
+do
+    sleep 1
+done
+
+# Check if the WordPress database already exists
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+
+    echo "Initializing MariaDB..."
+
+    mariadb -u root <<EOF
+CREATE DATABASE ${MYSQL_DATABASE};
+
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+
+GRANT ALL PRIVILEGES
+ON ${MYSQL_DATABASE}.*
+TO '${MYSQL_USER}'@'%';
+EOF
+
+else
+
+    echo "Database already exists."
+
 fi
 
+# Stop temporary MariaDB
+mysqladmin -u root shutdown
+
+# Start MariaDB normally
+exec mysqld --user=mysql
